74daca02f42e6330b8a27a50d38b75c3fdf12a42
Milvio LuA <milviolua@gmail.com>
2020-04-04 00:20:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GrabCabAdmin_code
+ 
---
+++ src/config/dev.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcxjL1i1DxGz4jfD1D_6ghHQPUWqwuhh8",
+  authDomain: "taxi-gotech.firebaseapp.com",
+  databaseURL: "https://taxi-gotech.firebaseio.com",
+  projectId: "taxi-gotech",
+  storageBucket: "taxi-gotech.appspot.com",
+  messagingSenderId: "907388765255",
+  appId: "1:907388765255:web:6e0c9acb9b0da4ad7d5d62",
+  measurementId: "G-NQNPR5YQRP"
+};
